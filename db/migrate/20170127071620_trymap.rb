75d40554350dd8a985ac3cf81520ddaa945270a8
Sarmad Sabih <sam_sarmad@hotmail.com>
2017-12-05 10:27:20 +0500

    PG removed

--- db/migrate/20170127071620_trymap.rb
+++
@@ -1,7 +0,0 @@
-class Trymap < ActiveRecord::Migration
-  def change
-  	   create_table :testmaps do |t|
-      	t.timestamps null: false
-      end
-  end
-end
--- db/migrate/20170127071828_addcolumn.rb
+++
@@ -1,7 +0,0 @@
-class Addcolumn < ActiveRecord::Migration
-  def change
-  		add_column :testmaps, :latitude, :float
-  		add_column :testmaps, :longitude, :float
-  		add_column :testmaps, :applicant, :string
-  end
-end
