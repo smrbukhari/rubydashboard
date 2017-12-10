45bd9967e2836654b89e257eb3bff676c91f46ea
Sarmad Sabih <sam_sarmad@hotmail.com>
2017-12-10 21:26:51 +0500

    map on bidashboard

--- app/assets/javascripts/exclusive/bidashboard.js
+++ app/assets/javascripts/exclusive/bidashboard.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
   BiSquare.FiltersHelper.handleFilterChange('filter2', 'filter2_options', 'filter2_options_select_all');
   BiSquare.FiltersHelper.handleSelectAllCheckAndUncheck('filter1_options_select_all', 'filter1_options');
   BiSquare.FiltersHelper.handleSelectAllCheckAndUncheck('filter2_options_select_all', 'filter2_options');
+  geo_function();
 
   $("#generate_plBarDiv1").click(function() {
     BiSquare.BiDashboardHelper.staticPlotGeneration({filter1Id: 'filter1', filter2Id: 'filter2', filter3Id: 'filter3', filter4Id: 'filter4',
